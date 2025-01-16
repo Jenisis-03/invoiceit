de512029540c9be347ba74900648b4ea7d5112cb
Subham Dubey <111039096+Jenisis-03@users.noreply.github.com>
2025-01-16 23:12:30 +0530

    Login Routes and Other Routes added with Dashboard Created

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,16 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Onboarding() {
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>You are almost there!</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
+}
+
+
+//https://youtu.be/AH3xlNuui_A?t=8393
